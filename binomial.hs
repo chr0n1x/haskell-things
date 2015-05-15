4bfc828707c8ca00869ce5b34163dfd6e85e29ab
chr0n1x <heilong24@gmail.com>
2015-05-14 22:50:00 -0400

    catalan & binomial: add fx signatures

--- binomial.hs
+++ binomial.hs
@@ -1,5 +1,6 @@
 module Binomial where
 
+binomial :: Int -> Int -> Int
 binomial n k
   | 0 <= k && k < n  = ((binomial (n-1) (k-1)) + (binomial (n-1) k))
   | k == 0 || n == k = 1
--- catalan.hs
+++ catalan.hs
@@ -2,4 +2,5 @@ module Catalan where
 
 import Binomial
 
+catalan :: Int -> Int
 catalan n = (binomial (2*n) n) - (binomial (2*n) (n+1))
